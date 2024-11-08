96897e63af4e1c6623a85c749265bc69cf4f0334
ElyAi <12211308@mail.sustech.edu.cn>
2024-11-08 23:19:37 +0800

    add animator for enemy and modify Enemy.cs

--- Assets/Resources/Scripts/Enemy.cs
+++ Assets/Resources/Scripts/Enemy.cs
@@ -1,114 +1,215 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class EnemyAiTutorial : MonoBehaviour
 {
+    NavMeshAgent agent; // 导航代理
+    Animator animator;
+    Transform player; // 玩家的Transform对象
+    public LayerMask whatIsPlayer; // 碰撞检测所需的LayerMask
+    [Header("敌人的生命值")]
+    public float health;
 
-    public enum EnemyState
+    [Space]
+    [Header("巡逻相关")]
+    [Header("巡逻范围")]
+
+    public float walkPointRange;
+    bool walkPointSet;//是否设置了巡逻点
+    Vector3 walkPoint;//巡逻点
+    bool isWalking;//是否正在移动
+    bool isRunning;//是否正在奔跑
+
+    [Space]
+    [Header("攻击相关")]
+    [Header("攻击间隔时间")]
+    public float timeBetweenAttacks;
+    [Header("每次攻击时间")]
+    public float timeAttack;
+    [Header("攻击所使用的物体(子弹)")]
+    public GameObject projectile;
+    bool isAttack;
+    bool alreadyAttacked;//是否已经攻击过
+
+    [Space]
+    [Header("状态相关")]
+    [Header("是否是近战攻击")]
+    public bool isMeleeAttack;
+    [Header("远程攻击点")]
+    public Transform remoteAttackPoint;
+    [Header("视野范围和攻击范围")]
+    public float sightRange;
+    public float attackRange;
+    //玩家是否在视野范围和攻击范围内
+    public bool playerInSightRange;
+    public bool playerInAttackRange;
+    [Header("速度")]
+    public float walkSpeed = 1.2f;
+    public float runSpeed = 3f;
+    bool isIdle;
+
+    private void Awake()
     {
-        NormalState,
-        FightingState,
-        MovingState,
-        RestingState
+        player = GameObject.FindWithTag("Player").transform; // 查找到名为"PlayerObj"的游戏物体，并获取其Transform组件
+        agent = GetComponent<NavMeshAgent>(); // 获取自身的导航代理组件
+        animator = GetComponent<Animator>();
+        Debug.Log(animator);
     }
 
-    private NavMeshAgent enemyAgent;
-    private EnemyState state = EnemyState.NormalState;
-    private EnemyState childState = EnemyState.RestingState;
-
-    public GameObject[] foodPrefabs;
-    public float restTime = 2;
-    private float restTimer = 0;
-
-    public int HP = 100;
-    public int maxHP = 100;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-        enemyAgent = GetComponent<NavMeshAgent>();
-        
+        // 检查玩家是否在视野范围或攻击范围内
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        // 根据玩家位置和状态进行相应的行为
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInAttackRange && playerInSightRange) AttackPlayer();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Patroling()
     {
-        //初始状态下分为休息状态和巡逻状态
-        if(state == EnemyState.NormalState)
+        if (alreadyAttacked)
         {
-            //自动切换休息状态和巡逻状态
-            if(childState == EnemyState.RestingState)
-            {
-                restTimer += Time.deltaTime;
+            return;
+        }
 
-                if(restTimer > restTime)
-                {
-                    Vector3 randomPositon = FindRandomPosition();
-                    enemyAgent.SetDestination(randomPositon);
-                    childState = EnemyState.MovingState;
-                }
-            }else if(childState == EnemyState.MovingState)
-            {
-                if(enemyAgent.remainingDistance <= 0)
-                {
-                    restTimer = 0;
-                    childState = EnemyState.RestingState;  
+        if (!walkPointSet) SearchWalkPoint(); // 如果没有设置巡逻点，则搜索巡逻点
 
-                }
+        if (walkPointSet)
+        {
+            agent.speed = walkSpeed; // 将导航代理的速度设置为5
+            //TODO设置巡逻速度，播放走路动画
+            animator.SetBool("Walk", true);
+            animator.SetBool("Run", false);
+            agent.SetDestination(walkPoint); // 设置巡逻点为目标点
+        }
+
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        // 已到达巡逻点
+        if (distanceToWalkPoint.magnitude < 1f)
+        {
+            //TODO:播放待机动画,休息3-5秒，也可以等待机动画执行完再进行下一步
+            animator.SetBool("Walk", false);
+            animator.SetBool("Run", false);
+
+            if (!isIdle)
+            {
+                Invoke(nameof(SetWalkPointSet), Random.Range(3, 6));
             }
-        }
-        //测试掉血
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            TakeDamage(20);
-        }
-  
 
+            isIdle = true;
+        }
     }
 
-    //生成二维随机方向
-    Vector3 FindRandomPosition()
+    void SetWalkPointSet()
     {
-        Vector3 randomDir = new Vector3(Random.Range(-1, 1f), 0, Random.Range(-1, 1f));
-        return transform.position + randomDir.normalized * Random .Range(2, 5);
+        isIdle = false;
+        walkPointSet = false;
+    }
+
+    private void SearchWalkPoint()
+    {
+        // 在指定范围内随机生成一个巡逻点
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+        // SamplePosition表示判断该点是不是导航允许到达的地方
+        NavMeshHit hit;
+        if (NavMesh.SamplePosition(walkPoint, out hit, walkPointRange, 1))
+        {
+            walkPoint = hit.position;
+            walkPointSet = true;
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        //判断是否在攻击
+        if (isAttack) return;
+
+        //TODO设置追击速度，播放奔跑动画
+        agent.speed = runSpeed; // 将导航代理的速度设置为5
+        animator.SetBool("Walk", false);
+        animator.SetBool("Run", true);
+        agent.SetDestination(player.position); // 设置导航代理的目标点为玩家位置
+     
+    }
+
+    private void AttackPlayer()
+    {
+        // 已到达目标点附近
+        if ((transform.position - player.position).magnitude < 1.5f)
+        {
+            // 停止移动
+            agent.SetDestination(transform.position);
+        }
+      
+        if (!alreadyAttacked)
+        {
+            //面向玩家
+            transform.LookAt(player);
+
+            //是否是近战攻击
+            if (isMeleeAttack)
+            {
+                animator.SetBool("Walk", false);
+                animator.SetBool("Run", false);
+                //TODO播放近战攻击动画
+                animator.SetTrigger("Attack");
+            }
+            else
+            {
+                ///攻击代码
+                Rigidbody rb = Instantiate(projectile, remoteAttackPoint.position, Quaternion.identity).GetComponent<Rigidbody>();
+                rb.AddForce(transform.forward * 32f, ForceMode.Impulse);
+                rb.AddForce(transform.up * 8f, ForceMode.Impulse);
+                ///攻击代码结束
+            }
+
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+
+            //攻击时间
+            isAttack = true;
+            animator.SetBool("isAttack", true);
+            Invoke(nameof(ResetIsAttack), timeAttack);
+        }
+    }
+
+    private void ResetIsAttack()
+    {
+        isAttack = false;
+        animator.SetBool("isAttack", false);
+    }
+
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
     }
 
     public void TakeDamage(int damage)
     {
-        HP -= damage;
-        Debug.Log("Enemy took damage. Current HP: " + HP);
+        health -= damage;
 
-        if (HP <= 0)
-        {
-            Die();
-        }
+        // 如果生命值小于等于0，则摧毁自身
+        //TODO: 播放死亡动画
+        if (health <= 0) Invoke(nameof(DestroyEnemy), 0.5f);
     }
 
-    public void Die()
+    private void DestroyEnemy()
     {
-        if (foodPrefabs.Length > 0)
-        {
-            // 随机选择一个食物预制体
-            int randomIndex = Random.Range(0, foodPrefabs.Length);
-            GameObject foodToDrop = foodPrefabs[randomIndex];
-
-            // 在敌人所在位置生成食物
-            Instantiate(foodToDrop, transform.position, Quaternion.identity);
-        }
-        EnemyManager enemyManager = FindObjectOfType<EnemyManager>(); // 查找 EnemyManager
-        if (enemyManager != null)
-        {
-            enemyManager.EnemyDied(this.gameObject); // 通知管理器该敌人已死亡
-        }
-
+        Destroy(gameObject);
     }
 
-    public void ResetEnemy(Vector3 spawnPosition)
+    private void OnDrawGizmosSelected()
     {
-        HP = maxHP; // 重置生命值
-        transform.position = spawnPosition; // 重置位置到新生成点
-        gameObject.SetActive(true); // 重新激活敌人
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, attackRange); // 在编辑器中绘制攻击范围的圆
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, sightRange); // 在编辑器中绘制视野范围的圆形
     }
 }
